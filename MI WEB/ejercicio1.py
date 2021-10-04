db7ac62e9c445efdb500c0a4442be39b607f94f2
fermatt95 <91813877+fermatt95@users.noreply.github.com>
2021-10-03 21:51:10 -0300

    Add files via upload

---
+++ MI WEB/ejercicio1.py
@@ -0,0 +1,13 @@
+USER = 'pep3'
+PASSWORD = '123'
+
+print('Proporciona los siguientes datos: ')
+user = input('Usuario: ')
+password = input ('Contraseña: ')
+
+if user == USER and password == PASSWORD:  
+    print('             insta [°]')
+    print('Bienvenidos')
+else:
+    print('Datos incorrectos, intenta de nuevo')
+
---
+++ MI WEB/examen.py
@@ -0,0 +1,20 @@
+hijos = input('tenes hijos')
+
+hermanos = input ('sos el hermano mayor')
+
+madre = input ('sos madre/padre')
+
+
+puntaje = input ('suma las afirmaciones')
+puntaje = int(puntaje) 
+if puntaje == 0:
+    print("sin puntos")
+elif puntaje <=2: 
+    print("poco puntaje")
+else: 
+    print("excelente puntaje")
+
+
+
+
+
